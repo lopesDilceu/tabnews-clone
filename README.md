0cf4c8d254458b01405e651424fe45453f0b9bce
lopesDilceu <dilceulopesjr181@gmail.com>
2026-04-08 23:33:39 -0300

    feat: commiting lint scripts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tabnews-clone
+
 Clone project of tabnew.com.br made on studying purpose
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-function Home () {
-    return [ 
-        <div>
-            <h1>Se há algum tipo de magia neste mundo, deve estar na tentativa de compreender alguém, de compartilhar algo.</h1>
-            <h2>Te amo, Julia Maria ❤️❤️</h2>
-        </div>
-    ]
+function Home() {
+  return [
+    <div>
+      <h1>
+        Se há algum tipo de magia neste mundo, deve estar na tentativa de
+        compreender alguém, de compartilhar algo.
+      </h1>
+      <h2>Te amo, Julia Maria ❤️❤️</h2>
+    </div>,
+  ];
 }
+
 
 export default Home;
