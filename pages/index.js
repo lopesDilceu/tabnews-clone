fe881349710f6a7146067604c312bdb031e1d75d
lopesDilceu <dilceulopesjr181@gmail.com>
2026-04-19 09:55:25 -0300

    refactor: updating index page to  page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
 function Home() {
   return [
     <div>
-      <h1>
-        Se há algum tipo de magia neste mundo, deve estar na tentativa de
-        compreender alguém, de compartilhar algo.
-      </h1>
-      <h2>Te amo, Julia Maria ❤️❤️</h2>
+      <h1>Em Construção...</h1>
     </div>,
   ];
 }
