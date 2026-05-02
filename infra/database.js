2919224b37f1c8d807195737cfdbe55e80ced97d
lopesDilceu <dilceulopesjr181@gmail.com>
2026-05-02 15:55:49 -0300

    feat:implementing error handling on database.js

--- infra/database.js
+++ infra/database.js
@@ -3,15 +3,21 @@ import { Client } from 'pg';
 async function query(queryObject) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_TESTE,
+    port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end(); 
+  }
 }
 
 export default {
